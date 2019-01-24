9f2f6ae0f3899517b0d0ec2b87db8a45d22912ee
Mattia Bernetti <[mbernett@sissa.it]>
2019-01-24 18:21:25 +0100

    Added details to compile plumed (and asmjit) on SISSA cluster.

--- install-gromacs.sh
+++ install-gromacs.sh
@@ -58,18 +58,43 @@ make install
 
 
 
-### This was used to install on SISSA cluster (frontend2):
+### This was used to install on frontend2 WITH PLUMED:
 
 source /etc/profile.d/modules.sh
 
-## module purge
-## module load intel/16.0
-## module load gcc/4.8.2
-## module load openmpi/1.8.5/intel/any
-## module load cudatoolkit/8.0
-# module load fftw/3.3.4
-# module load Cmake
-# module load Fftw/3.3.4
+module purge
+module load cmake/3.5.0
+
+# mbernett: this is to be consistent with the modules used to compile plumed 2.5s
+module load openmpi/1.8.3/intel/17.0
+# the above line implies automatically loading also:
+# module load intel/18.4
+# module load gnu/4.9.2
+
+module load cudatoolkit/10.0
+export CMAKE_PREFIX_PATH=/home/bussi/modules/fftw/3.3.4/intel
+
+installdir=/home/mbernett/modules/GROMACS/gromacs-2018.4
+umask 022
+
+mkdir build-sp
+cd build-sp
+
+cmake .. \
+    -DCMAKE_C_COMPILER:FILEPATH=$(which mpicc) \
+    -DCMAKE_CXX_COMPILER:FILEPATH=$(which mpic++) \
+    -DCMAKE_INSTALL_PREFIX:STRING=$installdir \
+    -DGMX_MPI=ON
+
+make -j 8
+
+make install
+cd ../
+
+
+### This was used to install on frontend2 WITHOUT PLUMED:
+
+source /etc/profile.d/modules.sh
 
 module purge
 module load cmake/3.5.0
